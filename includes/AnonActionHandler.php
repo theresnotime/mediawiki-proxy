14ffe9604df578b4ecb358c415abab4d1e3598e7
csteipp <csteipp@wikimedia.org>
2015-05-24 09:29:28 -0700

    Update root text

--- includes/AnonActionHandler.php
+++ includes/AnonActionHandler.php
@@ -6,24 +6,27 @@ class AnonActionHandler extends ActionHandler {
 
 	public function exec( User $user, array $request, Output &$output, Settings $config ) {
 
+		$wiki = $config->getWikiConfig();
+
 		$html = $output->getTemplateHtml(
 			'heading',
-			Array( 'text'=>'Use Tor to edit Wikipedia.' )
+			array( 'text'=>'Use Tor to edit Wikipedia.' )
 		);
 
 		$html .= $output->getTemplateHtml(
 			'paragraph',
-			Array( 'header'=>false, 'body' => "Intro paragraph" )
+			array(
+				'header'=>false, 'body' => "Tor exit node IP addresses are blocked by Wikipedia due to excessive spam from Tor. However, there are legitimate reasons to use Tor for editing Wikipedia. This proxy allows a group of trusted users to edit English Wikipedia from Tor." )
 		);
 
 		$html .= $output->getTemplateHtml(
 			'paragraph',
-			Array( 'header'=>'How it works', 'body' => "zxcv" )
+			Array( 'header'=>'How it works', 'body' => "You will first need to login to Wikipedia using your global identity, and authorize the TorProxy app to edit as you, using OAuth. After you request authorization to use this proxy, the proxy admin will need to approve your request. Your request will be posted publicly (via this proxy) to {$wiki['notification_page']}. Once you are authorized, you can use this proxy to edit an article at any time." )
 		);
 
 		$html .= $output->getTemplateHtml(
 			'paragraph',
-			Array( 'header'=>'Why privacy?', 'body' => "zxcv" )
+			Array( 'header'=>'Why privacy?', 'body' => "" )
 		);
 
 		$output->addTemplate( 'content', Array( 'html' => $html ) );
